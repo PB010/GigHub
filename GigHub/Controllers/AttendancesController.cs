39f9e9b6a5d855dcfc26555b37532bde70b10e90
Petru Betco <petrubetco@gmail.com>
2019-05-14 19:28:19 +0300

    Created Attendances api controller and added ajax buttons

---
+++ GigHub/Controllers/AttendancesController.cs
@@ -0,0 +1,39 @@
+﻿using System.Linq;
+using GigHub.Models;
+using Microsoft.AspNet.Identity;
+using System.Web.Http;
+using GigHub.Dtos;
+
+namespace GigHub.Controllers
+{
+    [Authorize]
+    public class AttendancesController : ApiController
+    {
+        private readonly ApplicationDbContext _context;
+
+        public AttendancesController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        [HttpPost]
+        public IHttpActionResult Attend(AttendanceDto attendanceDto)
+        {
+            var userId = User.Identity.GetUserId();
+
+            if (_context.Attendances
+                .Any(a => a.AttendeeId == userId && a.GigId == attendanceDto.GigId))
+                return BadRequest("Attendance already exists.");
+
+            var attendance = new Attendance
+            {
+                GigId = attendanceDto.GigId,
+                AttendeeId = userId
+            };
+            _context.Attendances.Add(attendance);
+            _context.SaveChanges();
+
+            return Ok();
+        }
+    }
+}
