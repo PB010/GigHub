b5206dcb51feda7bd492cfaeb4aa17ceb7c381a4
Petru Betco <petrubetco@gmail.com>
2019-05-17 18:22:48 +0300

    Added notification read action in controller, and jquery to modify the notifcation ui

--- GigHub/Controllers/api/NotificationsController.cs
+++ GigHub/Controllers/api/NotificationsController.cs
@@ -34,5 +34,21 @@ namespace GigHub.Controllers.Api
 
             return notifications.Select(Mapper.Map<Notification, NotificationDto>);
         }
+
+        [HttpPost]
+        public IHttpActionResult NotificationsRead()
+        {
+            var userId = User.Identity.GetUserId();
+            var notifications = _context.UserNotifications
+                .Where(un => un.UserId == userId && !un.IsRead)
+                .ToList();
+
+            notifications.ForEach(n => n.ReadNotification());
+
+            _context.SaveChanges();
+
+            return Ok();
+
+        }
     }
 }
