406dfa884dab819b95003d7ce42ef3f47b3a0748
Petru Betco <petrubetco@gmail.com>
2019-05-18 20:01:28 +0300

    Added api to remove attendance

--- GigHub/Controllers/api/AttendancesController.cs
+++ GigHub/Controllers/api/AttendancesController.cs
@@ -1,8 +1,8 @@
-﻿using System.Linq;
-using System.Web.Http;
-using GigHub.Dto;
+﻿using GigHub.Dto;
 using GigHub.Models;
 using Microsoft.AspNet.Identity;
+using System.Linq;
+using System.Web.Http;
 
 namespace GigHub.Controllers.api
 {
@@ -34,6 +34,22 @@ namespace GigHub.Controllers.api
             _context.SaveChanges();
 
             return Ok();
+        }
+
+        [HttpDelete]
+        public IHttpActionResult DeleteAttendance(int id)
+        {
+            var userId = User.Identity.GetUserId();
+            var attendance = _context.Attendances
+                .SingleOrDefault(a => a.GigId == id && a.AttendeeId == userId);
+
+            if (attendance == null)
+                return NotFound();
+
+            _context.Attendances.Remove(attendance);
+            _context.SaveChanges();
+
+            return Ok(id);
         }
     }
 }
